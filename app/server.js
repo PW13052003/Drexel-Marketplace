26570088066f501765f468506ef33c21189197b6
brittyb <124463698+brittyb@users.noreply.github.com>
2025-12-07 19:28:59 -0500

    change server.js

--- app/server.js
+++ app/server.js
@@ -21,8 +21,8 @@ const io = new Server(server);
 const argon2 = require("argon2");
 app.use(express.json());
 
-const { v4: uuidv4 } = require('uuid');
-//const uuidv4 = (...args) => import('uuid').then(({ v4 }) => v4(...args));
+//const { v4: uuidv4 } = require('uuid');
+const uuidv4 = (...args) => import('uuid').then(({ v4 }) => v4(...args));
 
 const fileUpload = require('express-fileupload');
 
